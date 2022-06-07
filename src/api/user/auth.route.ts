34d9266d1b22b00dd806e6301004be7854f141de
navitas28 <anshulsaxena2807@gmail.com>
2022-06-07 12:55:07 +0530

    WIP: authentication

--- src/api/user/auth.route.ts
+++ src/api/user/auth.route.ts
@@ -1,15 +1,15 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import express, {Request, Response} from 'express';
 import {body} from 'express-validator';
 import {Magic} from '@magic-sdk/admin';
 import passport from 'passport';
-const MagicStrategy = require('passport-magic').Strategy;
+import {Strategy as MagicStrategy} from 'passport-magic';
 
 import {User} from './user.model';
 
 const router = express.Router();
 
-const magic = new Magic(process.env.MAGIC_SECRET_KEY!);
+const magic = new Magic(process.env.MAGIC_SECRET_KEY);
 
 const strategy = new MagicStrategy(async function (user: any, done: any) {
 	const userMetadata = await magic.users.getMetadataByIssuer(user.issuer);
@@ -17,35 +17,33 @@ const strategy = new MagicStrategy(async function (user: any, done: any) {
 	const existingUser = await User.findOne({issuer: user.issuer});
 	if (!existingUser) {
 		/* Create new user if doesn't exist */
-		// return signup(user, userMetadata, done);
+		return signup(user, userMetadata, done);
 	} else {
 		/* Login user if otherwise */
-		return '';
+		return login();
 	}
 });
 
 passport.use(strategy);
-/*
-const signup = async (user, userMetadata, done) => {
-	// let newUser = {
-	//   issuer: user.issuer,
-	//   email: userMetadata.email,
-	//   lastLoginAt: user.claim.iat
-	// };
-	// await users.insert(newUser);
-	// return done(null, newUser);
+const signup = async (user: any, userMetadata: any, done: any) => {
+	// 	// let newUser = {
+	// 	//   issuer: user.issuer,
+	// 	//   email: userMetadata.email,
+	// 	//   lastLoginAt: user.claim.iat
+	// 	// };
+	// 	// await users.insert(newUser);
+	// 	// return done(null, newUser);
 	return '';
-}; */
+};
+
+const login = () => {
+	return 'Logged In';
+};
 
 router.post(
-	'/api/authenticate',
-	[body('email').trim().isEmail().withMessage('Invalid Email')],
+	'/api/login',
+	body('email').trim().isEmail().withMessage('Please provide correct email'),
 	passport.authenticate('magic'),
-	async (req: Request, res: Response) => {
-		const {email} = req.body;
-
-		res.status(201).json({message: 'Route Active'});
-	},
 );
 
 export {router as authRouter};
