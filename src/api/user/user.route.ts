f61a760ba9504bc9f57e6b4357023a7aaa6f952f
Prajjawalk <prajjawalkhandelwal@gmail.com>
2022-07-09 18:08:47 +0530

    added model for userdata

--- src/api/user/user.route.ts
+++ src/api/user/user.route.ts
@@ -1,8 +1,11 @@
 import express, {Request, Response} from 'express';
 import {User} from '../../models/user.model';
+import { User_data } from '../../models/user-data.model';
+
 
 const router = express.Router();
 import {Magic, SDKError} from '@magic-sdk/admin';
+
 
 router.get('/api/user/update-email', async (req: Request, res: Response) => {
     let email = req.query.email
@@ -23,5 +26,13 @@ router.get('/api/user/update-email', async (req: Request, res: Response) => {
         res.status(404).send("User not found")
     }
 });
+
+router.post('/api/user/data', async (req: Request, res: Response) => {
+    let data = req.body;
+	console.log(req.body)
+	let mongoData = await User_data.create(data) 
+	console.log(mongoData)
+    res.send({data: mongoData})
+})
 
 export{router as userRouter}
